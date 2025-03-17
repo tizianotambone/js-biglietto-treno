9cc0533e8617107caf29aa49d25096e39e102cfd
tizianotambone <tambonetiziano@gmail.com>
2025-03-17 14:25:58 +0100

    [UPD] use-if-else

--- javascript/script.js
+++ javascript/script.js
@@ -1,25 +1,27 @@
 let età = parseInt(prompt("indica l'età"));
-console.log ("età");
+console.log (età);
 
 let chilometri = parseFloat(prompt("indicare i chilometri da percorrere"));
-console.log ("chilometri");
+console.log (chilometri);
 
 // costo x chilometro
 
-const costoalkm = 0,21;
+const costoalkm = 0.21;
 // calcolo del costo base senza sconti
 
 let costobase = chilometri * costoalkm
-console.log =("costobase")
+console.log (costobase)
 // se luntente  è compreso tra 18e 64 anni non ottiene sconto
 
 let sconto = 0;
+
 console.log(sconto)
 
+
 if (età < 18){
-    sconto =  costoalkm - costoalkm * 20 / 100
+    sconto =   costobase * 20 / 100
 }
- else (età > 64){
-    sconto =  costoalkm - costoalkm * 40 / 100
+ else if (età > 64){
+    sconto =  costobase * 40 / 100
  }
 
