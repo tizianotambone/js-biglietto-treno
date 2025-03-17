c90b479b4e717f35badbedcd6741e2074a13e932
tizianotambone <tambonetiziano@gmail.com>
2025-03-17 14:05:36 +0100

    [UPD] incazione età

--- javascript/script.js
+++ javascript/script.js
@@ -7,5 +7,13 @@ console.log ("chilometri");
 const costoalkm = 0,21;
 
 let costobase = chilometri * costoalkm
+console.log =("costobase")
 
+let sconto = 0;
+if (età < 18){
+    sconto =  costoalkm - costoalkm * 20 /100
+}
+ else (età > 64){
+    sconto =
+ }
 
